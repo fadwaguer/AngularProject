7d8e23b8b4c4b3ebb0686a68b975ef09e697e167
Fadwa Guermane <f.guermane@charvet-digitalmedia.com>
2023-12-28 10:23:22 +0100

    set up backend

---
+++ api/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { url } from 'inspector';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: false,
+
+    } )
+  ],
+  controllers: [AppController],
+  providers: [AppService]
+
+})
+export class AppModule  {}
